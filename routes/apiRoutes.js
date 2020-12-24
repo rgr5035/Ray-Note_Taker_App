acd0e2fe9394df11635384f65f84bd16638b8f2b
Robby Ray <rgr5035@gmail.com>
2020-12-23 17:42:02 -0700

    added delete function to notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -25,7 +25,7 @@ module.exports = (app) => {
 
             text = JSON.parse(text);
             req.body.id = Date.now();
-            text.push(req.body);
+            text.push(req.body); //req.body represents what is sent over in a post request
 
             fs.writeFile(__dirname + "/../db/db.json", JSON.stringify(text), error => {
                 if (error) {
@@ -36,4 +36,39 @@ module.exports = (app) => {
             })
         })
     })
+
+    //API DELETE Request
+    app.delete('/api/notes/:id', (req, res) => { //creates a parameter on the req.params
+        fs.readFile(__dirname + "/../db/db.json", "utf8", (error, text) => {
+            if (error) {
+               console.log(error) 
+            }
+
+            text = JSON.parse(text);
+
+            const filteredArray = [];
+            const id = req.params.id;
+            for (let i = 0; i < text.length; i++) {
+                const currentNoteId = text[i].id;
+
+                console.log(currentNoteId);
+
+                if (currentNoteId != id) {
+                    filteredArray.push(text[i]);    
+                } 
+            }
+
+                console.log(filteredArray);
+            
+            let filteredNotes = JSON.stringify(filteredArray);
+
+            fs.writeFile(__dirname + "/../db/db.json", filteredNotes, error => {
+                if (error) {
+                    console.log(error)
+
+                }
+                res.json(true);
+            })
+    });
+});
 };
